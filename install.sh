11913ab602868a6c0e2abb7de0a1f92cba50dfff
Alexandra Bugariu <alexandra.bugariu@inf.ethz.ch>
2023-09-28 11:19:06 +0200

    added pysmt and smt-triggen

--- install.sh
+++ install.sh
@@ -7,16 +7,15 @@
 #*******************************************************************************
 #!/bin/bash
 
-ROOT=~/unsaty
-
+ROOT=~/SMT
 PATH_SMTSolvers=$ROOT/SMTSolvers
 PATH_UNSATY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 
-mkdir $ROOT
+mkdir -p $ROOT
 cd $ROOT
 
 # download and build the Z3 version used in our experiments in the folder SMTSolvers
-mkdir $PATH_SMTSolvers
+mkdir -p $PATH_SMTSolvers
 cd $PATH_SMTSolvers
 
 # Z3
@@ -28,6 +27,12 @@ cd z3_4.12.1
 python scripts/mk_make.py --java
 cd build
 make
+
+# clone smt-triggen and install PySMT
+cd $ROOT
+git clone https://github.com/alebugariu/smt-triggen Triggen 
+curl -L https://github.com/alebugariu/pysmt/archive/refs/tags/v0.8.0-ale.tar.gz | tar xvz -C .
+mv pysmt-0.8.0-ale pysmt
 
 # compile our tool:
 cd $PATH_UNSATY
