bc5bc345b0467411ba9e58135312a15c7d078a27
bugariua <alexandra.bugariu@inf.ethz.ch>
2023-11-16 10:15:32 +0100

    used the outer most quantifier when collecting the patterns

--- src/proof_analyser/Input_Reader.java
+++ src/proof_analyser/Input_Reader.java
@@ -464,9 +464,10 @@ public class Input_Reader {
 			List<List<String>> patterns_as_strings;
 			String quantifier_as_string = quantifier.toString();
 			int index_var = quantifier_as_string.lastIndexOf(":var");
-			if (!(index_var == -1 || quantifier_as_string.indexOf(":pattern") > index_var)) {
-				Quantifier direct_parent = parent_quantifiers.get(parent_quantifiers.size() - 1);
-				String parent_as_string = String_Utility.remove_line_breaks(direct_parent.toString());
+			int index_pattern = quantifier_as_string.indexOf(":pattern");
+			if (!(index_var == -1 || index_pattern > index_var)) {
+				Quantifier outer_most_parent = parent_quantifiers.get(0);
+				String parent_as_string = String_Utility.remove_line_breaks(outer_most_parent.toString());
 				int quant_vars_start_index = quantifier_as_string.indexOf("((");
 				int quant_vars_end_index = String_Utility.match_brackets(quantifier_as_string, quant_vars_start_index);
 				quantifier_as_string = "(forall "
